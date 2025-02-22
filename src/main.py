943910283a381d10d43c9fc77bf86d48c8e693ec
justabloob <justabloob@gmail.com>
2025-02-23 00:03:37 +0100

    added tests

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("This is a textnode", TextType.ITALIC, "https://www.google.com")
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,15 +1,21 @@
 from enum import Enum
 
-class Texttype(Enum):
-    NORMAL_TEXT = text
-    BOLD_TEXT = **{text}**
-    ITALIC_TEXT = *{text}*
-    CODE_TEXT = `{text}`
-    LINKS = [text](url)
-    IMAGES = ![text](url)
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINKS = "links"
+    IMAGES = "images"
 
-class TextNode(Texttype):
-    def __init__(self, text, url=None):
-        self.text_type = Texttype
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text_type = text_type
         self.text = text
         self.url = url
+    
+    def __eq__(self, other):
+        return self.text == other.text and self.url == other.url and self.text_type == other.text_type
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
