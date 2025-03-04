3af6c847dd3d0b5e8bbae0e651a4cb27d7d24b02
justabloob <justabloob@gmail.com>
2025-03-04 14:13:34 +0100

    added textnodetohtmlnode and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,5 @@
 import unittest
-
-from textnode import TextNode, TextType
-
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -35,6 +33,19 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.LINK, None)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "https://www.boot.dev", "alt": "This is an image"})
 
 
 if __name__ == "__main__":
