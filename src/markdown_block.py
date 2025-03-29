478476294f047f042afec51faa0cf938ce110fc2
justabloob <justabloob@gmail.com>
2025-03-29 21:00:22 +0100

    text to children and working on blocktohtml and markdown to html

--- src/markdown_block.py
+++ src/markdown_block.py
@@ -1,5 +1,7 @@
 from enum import Enum
-
+from htmlnode import HTMLNode, LeafNode, ParentNode
+from textnode import TextNode, text_node_to_html_node
+from markdown_inline import text_to_textnodes
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -24,29 +26,24 @@ def block_to_block_type(block):
     # Check if the block is empty
     if not block.strip():
         return BlockType.PARAGRAPH
-    
     # Check if the block is a heading
     if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
         return BlockType.HEADING
-    
     # Check if the block is a code block
     if block.startswith('```') and block.endswith('```'):
         return BlockType.CODE
-    
     # Check if the block is a quote
     if block.startswith('>'):
         for line in lines:
             if not line.startswith('>'):
                 return BlockType.PARAGRAPH
         return BlockType.QUOTE
-    
     # Check if the block is an unordered list
     if block.startswith('- '):
         for line in lines:
             if not line.startswith('- '):
                 return BlockType.PARAGRAPH
         return BlockType.UNORDERED_LIST
-    
     # Check if the block is an ordered list
     if block.startswith("1. "):
         i = 1
@@ -55,6 +52,34 @@ def block_to_block_type(block):
                 return BlockType.PARAGRAPH
             i += 1
         return BlockType.ORDERED_LIST
-    
     # If none of the above, return paragraph type
     return BlockType.PARAGRAPH
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_block(markdown) # split markdown into blocks
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        pass
+
+def text_to_children(text):
+    # Convert the text to a list of TextNode objects
+    text_nodes = text_to_textnodes(text)
+    # Convert the TextNode objects to LeafNode objects
+    children_nodes = []
+    for text_node in text_nodes:
+        children_node = text_node_to_html_node(text_node)
+        children_nodes.append(children_node)
+    return children_nodes
+
+    
+def block_to_html_node(block, block_type):
+    # Convert the block to an HTML node based on its type
+    if block_type == BlockType.PARAGRAPH:
+        return HTMLNode(tag="<p>", value=block)
+    elif block_type == BlockType.HEADING:
+        level = block.count("#")
+        tag = f"<h{level}>"
+        value = block[level:].strip()
+        return HTMLNode(tag=tag, value=value)
+    elif block_type == BlockType.CODE:
+        pass
