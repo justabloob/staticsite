6cb3e2fed560b60ce7aef987457865f865d323f6
justabloob <justabloob@gmail.com>
2025-04-10 18:47:55 +0200

    generate page added

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,3 +1,6 @@
+from markdown_block import markdown_to_html_node
+import os
+
 def extract_title(markdown):
     lines = markdown.split('\n')
     title = ""
@@ -6,3 +9,29 @@ def extract_title(markdown):
             title = line[2:].strip()
             break
     return title
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    # read the markdown file and store contents in a variable
+    markdown_file = open(from_path, "r")
+    markdown = markdown_file.read()
+    # read the template file and store contents in a variable
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    # convert markdown to html
+    html_node = markdown_to_html_node(markdown)
+    html_string = html_node.to_html()
+    # grab the title
+    title = extract_title(markdown)
+    # replace the title and content in the template
+    # with the html and title generated
+    final_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    # write new full html page to the destination path
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    dest_file = open(dest_path, "w")
+    dest_file.write(final_html)
+    # close all files
+    markdown_file.close()
+    template_file.close()
+    dest_file.close()
+    print(f"Page generated at {dest_path}")
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from textnode import *
 from copystatic import copy_files_recursive
+from generate_page import generate_page
 
 def main():
     copy_files_recursive("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
