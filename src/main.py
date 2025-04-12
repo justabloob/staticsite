bb9bb48e1953381d987ed2c7b9317879d41ef229
justabloob <justabloob@gmail.com>
2025-04-12 15:39:59 +0200

    small changes

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    print("Generating page...")
+    print("Generating content...")
     generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 
